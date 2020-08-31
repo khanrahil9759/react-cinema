dcf87154efbcf2792ce310853235d9a2f2e7bcac
rahil khan <khanrahil97591@gmail.com>
2020-08-31 16:39:28 +0530

    slideshow component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
+import './App.scss';
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <div className="app">
+        <Main />
+      </div>
     </Provider>
 
   );
